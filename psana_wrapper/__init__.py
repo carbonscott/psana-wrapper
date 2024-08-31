9648bd1e7e7ab755bd046c1b14937b82be600152
Cong Wang <cwang31@stanford.edu>
2024-08-30 23:40:26 -0700

    Format function arguments; Correct event fetching.

--- psana_wrapper/__init__.py
+++ psana_wrapper/__init__.py
@@ -15,13 +15,6 @@ class BasePsanaWrapper(ABC):
         self.exp = exp
         self.run = run
         self.detector_name = detector_name
-        self.detector = Detector(detector_name)
-        self.read = {
-            "raw"  : self.detector.raw,
-            "calib": self.detector.calib,
-            "image": self.detector.image,
-            "mask" : self.detector.mask,
-        }
 
     def create_bad_pixel_mask(self) -> np.ndarray:
         return self.read["mask"](self.run_current, calib=True, status=True, edges=True,
@@ -35,6 +28,14 @@ class PsanaWrapperIdx(BasePsanaWrapper):
         self.run_current   = next(self.datasource.runs())
         self.events        = self.run_current.times()
 
+        self.detector = Detector(detector_name)
+        self.read = {
+            "raw"  : self.detector.raw,
+            "calib": self.detector.calib,
+            "image": self.detector.image,
+            "mask" : self.detector.mask,
+        }
+
     def __len__(self) -> int:
         return len(self.events)
 
@@ -44,14 +45,22 @@ class PsanaWrapperIdx(BasePsanaWrapper):
         timestamp = self.events[event_num]
         return self.run_current.event(timestamp)
 
-    def get(self, event_num: int, id_panel: Optional[int] = None,
-            mode: ImageRetrievalMode = ImageRetrievalMode.calib) -> np.ndarray:
+    def get(
+            self,
+            event_num: int,
+            id_panel : Optional[int]      = None,
+            mode     : ImageRetrievalMode = ImageRetrievalMode.calib,
+        ) -> np.ndarray:
         event = self.get_event(event_num)
         data = self.read[mode](event)
         return data[id_panel] if id_panel is not None else data
 
-    def assemble(self, multipanel: bool = None, mode: ImageRetrievalMode = ImageRetrievalMode.image,
-                 event_num: int) -> np.ndarray:
+    def assemble(
+            self,
+            multipanel: bool               = None,
+            mode      : ImageRetrievalMode = ImageRetrievalMode.image,
+            event_num : int                = 0,
+        ) -> np.ndarray:
         event = self.get_event(event_num)
         return self.read[mode](event, multipanel)
 
@@ -60,13 +69,24 @@ class PsanaWrapperSmd(BasePsanaWrapper):
         super().__init__(exp, run, detector_name)
         self.datasource_id = f"exp={exp}:run={run}:smd"
         self.datasource    = MPIDataSource(self.datasource_id)
-        self.events        = self.datasource.times()
+        self.events        = self.datasource.events()
+
+        self.detector = Detector(detector_name)
+        self.read = {
+            "raw"  : self.detector.raw,
+            "calib": self.detector.calib,
+            "image": self.detector.image,
+            "mask" : self.detector.mask,
+        }
 
     def __len__(self) -> int:
         return len(self.events)
 
-    def iter_events(self, mode: ImageRetrievalMode = ImageRetrievalMode.calib,
-                    id_panel: Optional[int] = None) -> Iterable[np.ndarray]:
-        for event in self.events():
+    def iter_events(
+            self,
+            mode: ImageRetrievalMode = ImageRetrievalMode.calib,
+            id_panel: Optional[int] = None,
+        ) -> Iterable[np.ndarray]:
+        for event in self.events:
             data = self.read[mode](event)
             yield data[id_panel] if id_panel is not None else data
